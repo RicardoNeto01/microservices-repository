9f85e349ac69c75a47bb4c42609b5bb5cb9fff58
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-28 09:16:05 -0300

    feat: add PagamentoRepositoryTest

---
+++ ms-pagamentos/src/test/java/com/github/RicardoNeto01/ms_pagamentos/repository/PagamentoRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.github.RicardoNeto01.ms_pagamentos.repository;
+
+import com.github.RicardoNeto01.ms_pagamentos.entity.Pagamento;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+@DataJpaTest
+public class PagamentoRepositoryTest {
+
+    @Autowired
+    private PagamentoRepository repository;
+
+    @Test
+    public void deleteShouldDeleteObjectWhenIdExists() {
+        // Arrange
+        Long existingId = 1L;
+        //Act
+        repository.deleteById(existingId);
+        //Assert
+        Optional<Pagamento> result = repository.findById(existingId);
+        //Testa se existe um obj dentro do optional
+        Assertions.assertFalse(result.isPresent());
+    }
+}
