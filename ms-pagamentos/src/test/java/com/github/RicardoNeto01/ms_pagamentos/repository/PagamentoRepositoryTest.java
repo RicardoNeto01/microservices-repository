5b0efc13026426c39ae1aedf0061b5759ee886c2
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-28 09:46:19 -0300

    feat: add new methods to the PagamentoRepositoryTest

--- ms-pagamentos/src/test/java/com/github/RicardoNeto01/ms_pagamentos/repository/PagamentoRepositoryTest.java
+++ ms-pagamentos/src/test/java/com/github/RicardoNeto01/ms_pagamentos/repository/PagamentoRepositoryTest.java
@@ -1,7 +1,10 @@
 package com.github.RicardoNeto01.ms_pagamentos.repository;
 
 import com.github.RicardoNeto01.ms_pagamentos.entity.Pagamento;
+import com.github.RicardoNeto01.ms_pagamentos.tests.Factory;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -14,15 +17,45 @@ public class PagamentoRepositoryTest {
     @Autowired
     private PagamentoRepository repository;
 
+    private Long existingId;
+    private Long nonExistingId;
+    private Long countTotalPagamento;
+
+    @BeforeEach
+    void setup() throws Exception{
+        //Arrange
+        existingId = 1L;
+        nonExistingId = 100L;
+        countTotalPagamento = 3L;
+    }
+
+
     @Test
     public void deleteShouldDeleteObjectWhenIdExists() {
-        // Arrange
-        Long existingId = 1L;
         //Act
         repository.deleteById(existingId);
         //Assert
+        //Após deletar, tenta encontrar o objeto com o ID do repositório.
         Optional<Pagamento> result = repository.findById(existingId);
         //Testa se existe um obj dentro do optional
+        /*
+        Verifica se o objeto não está presente no repositório.
+        Se result.isPresent() for false, significa que foi deletado com sucesso.
+        */
+
         Assertions.assertFalse(result.isPresent());
+    }
+
+    @Test
+    @DisplayName("Dado parâmetros validos e id Nulo "
+            + "quando chamar CriarPagamento " +
+             "então deve instanciar um pagamento")
+    public void givenValidParamsAndIdIsNull_whenCallCreatePagamento_thenInstantiateAPagamento(){
+        Pagamento pagamento = Factory.creatPagamento();
+        pagamento.setId(null);
+        pagamento = repository.save(pagamento);
+        Assertions.assertNotNull(pagamento.getId());
+        //Verifica se o Id gerado é o próximo
+        Assertions.assertEquals(countTotalPagamento + 1 , pagamento.getId());
     }
 }
